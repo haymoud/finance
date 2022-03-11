3d6cce9a670bcf849522efd3d4129a0a760a3129
haymoud <82685342+haymoud@users.noreply.github.com>
2022-03-11 18:19:27 +0000

    fix sql 4

--- app.py
+++ app.py
@@ -53,11 +53,9 @@ def index():
     except:
         return apology("cannot execute")"""
 
-    try:
-        row1 = db.execute(
+
+    row1 = db.execute(
         "SELECT  cash, symbol, company_name, price_symbol, date_buy FROM users, buy_info WHERE id = ? GROUP BY (buy_info.symbol, users.cash)", session["user_id"])
-    except:
-        return apology("row1 error line 57")
 
     try:
         symbols = db.execute("SELECT DISTINCT(symbol) FROM users, buy_info WHERE id = ?", session["user_id"])
