16d57cfec4e21e31d134d91565a86cc3846d7442
haymoud <82685342+haymoud@users.noreply.github.com>
2022-03-11 13:57:23 +0000

    second try f

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def index():
     try:
         db.execute("CREATE TABLE IF NOT EXISTS buy_info (id_user, symbol TEXT NOT NULL, company_name TEXT NOT NULL, price_symbol REAL,date_buy TEXT NOT NULL, number_shares INTEGER, FOREIGN KEY(id_user) REFERENCES users(id))")
     except:
-        return apology("cannot execute)
+        return apology("cannot execute")
 
     row1 = db.execute(
         "SELECT  cash, symbol, company_name, price_symbol, date_buy FROM users, buy_info WHERE id = id_user GROUP BY (symbol)")
